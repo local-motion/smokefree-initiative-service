b4f0d43faa479250db8c5ff628ee51c064729409
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-11-14 11:42:57 +0100

    Add remaining and total to progress

--- src/main/java/smokefree/projection/Playground.java
+++ src/main/java/smokefree/projection/Playground.java
@@ -1,13 +1,17 @@
 package smokefree.projection;
 
 import lombok.Value;
+import lombok.experimental.Wither;
 import smokefree.domain.Status;
 
 @Value
+@Wither
 public class Playground {
     String id;
     String name;
     Double lat;
     Double lng;
     Status status;
+    int volunteerCount;
+    int votes;
 }
