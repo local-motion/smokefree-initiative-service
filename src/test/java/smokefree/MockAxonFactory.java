974d1bcea9b0a2b3e3213d1d67dc1bda727e76fe
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-11-16 13:01:32 +0100

    Improve testability of API to allow Owasp vulnerabilities verification

--- src/test/java/smokefree/MockAxonFactory.java
+++ src/test/java/smokefree/MockAxonFactory.java
@@ -5,21 +5,31 @@ import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Replaces;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.SimpleCommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.config.Configuration;
 import org.axonframework.config.DefaultConfigurer;
+import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+import org.axonframework.modelling.command.*;
+import org.axonframework.queryhandling.SimpleQueryBus;
+
+import javax.inject.Singleton;
 
 @Slf4j
 @Factory
 @Replaces(factory = AxonFactory.class)
 @NoArgsConstructor
 public class MockAxonFactory {
-    @Bean
+    @Singleton
     public Configuration configuration() {
         Configuration config = DefaultConfigurer
                 .defaultConfiguration()
                 .configureEmbeddedEventStore(c -> new InMemoryEventStorageEngine())
+                .configureCommandBus(c -> SimpleCommandBus.builder().build())
+                .configureEventBus(c -> SimpleEventBus.builder().build())
+                .configureQueryBus(c -> SimpleQueryBus.builder().build())
+
                 .buildConfiguration();
         config.start();
         return config;
