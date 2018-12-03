bb199fcb4d2d6d416ab3d4620293293e37d139bd
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-12-03 13:44:08 +0100

    Require JWT token when importing playgrounds
    local-motion/product#120

--- src/main/java/smokefree/PlaygroundImportController.java
+++ src/main/java/smokefree/PlaygroundImportController.java
@@ -1,10 +1,13 @@
 package smokefree;
 
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.AuthenticationException;
+import io.micronaut.security.utils.SecurityService;
+import org.axonframework.commandhandling.GenericCommandMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.config.Configuration;
 import smokefree.domain.CreateInitiativeCommand;
@@ -15,22 +18,37 @@ import javax.inject.Inject;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
+import static java.util.Collections.singletonMap;
+
+@Secured(IS_AUTHENTICATED)
 @Controller("/playgrounds")
 public class PlaygroundImportController {
     @Inject
+    Configuration configuration; // TODO: Hack for triggering Axon bootstrap
+
+    @Inject
     CommandGateway commandGateway;
     @Inject
-    Configuration configuration;
+    SecurityService securityService;
     @Inject
     InitiativeProjection initiativeProjection;
 
-    @Post(value="/", consumes= MediaType.APPLICATION_JSON)
+    @Post
     public String importPlayground(@Size(max=4096) @Body CreateInitiativeCommand cmd) {
-        return commandGateway.sendAndWait(cmd);
+        return commandGateway.sendAndWait(decorateWithUserId(cmd));
     }
 
     @Get
     public Collection<Playground> playgrounds() {
         return initiativeProjection.playgrounds();
+    }
+
+    private String requireUserId() {
+        return securityService.username().orElseThrow(() -> new AuthenticationException("Username required"));
+    }
+
+    private GenericCommandMessage<?> decorateWithUserId(Object cmd) {
+        return new GenericCommandMessage<>(cmd, singletonMap("user_id", requireUserId()));
     }
 }
