87a5b81cfd9a3a33026accfeac892c666e4c8f9c
Dimitri Dekker <dimitri_dekker@hotmail.com>
2019-02-28 12:42:14 +0100

    managers are projected in the list of volunteers (#60)
    revolves #231

--- src/main/java/smokefree/domain/ManagerJoinedInitiativeEvent.java
+++ src/main/java/smokefree/domain/ManagerJoinedInitiativeEvent.java
@@ -2,6 +2,11 @@ package smokefree.domain;
 
 import lombok.*;
 
+/**
+ * Note that the manager may or may not be a volunteer for this workspace. However any manager is considered a volunteer
+ * of the initiative and will be reflected as such in the projections. So there is no need to issue a JoinInitiativeCommand
+ * prior to this command.
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
