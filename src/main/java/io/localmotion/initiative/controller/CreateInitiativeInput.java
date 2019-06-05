eca37ca8ed8fdcee5cd7c705110c6e83a9c76e98
Dimitri Dekker <dimitri_dekker@hotmail.com>
2019-06-05 21:19:46 +0200

    small validation update

--- src/main/java/io/localmotion/initiative/controller/CreateInitiativeInput.java
+++ src/main/java/io/localmotion/initiative/controller/CreateInitiativeInput.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -21,10 +22,11 @@ class CreateInitiativeInput {
     @Size(min = 1, message = "The initiativeId must not be blank")
     String initiativeId;
 
-//    @Length(min = 3, message = "The name must be at least 3 characters")
-//    @Length(max = 40, message = "The name must be less than 40 characters")
-    @Pattern(regexp="^[ a-zA-Z0-9&'!\\-]{3,40}$")
-//    @NotNull
+    @NotBlank
+    @Length(min = 3, message = "The name must be at least 3 characters")
+    @Length(max = 40, message = "The name must be less than 40 characters")
+//    @Pattern(regexp="^[a-zA-Z0-9&'!\\-][ a-zA-Z0-9&'!\\-]{0,38}[a-zA-Z0-9&'!\\-]$") // only the middle characters may be a space
+    @Pattern(regexp="^([a-zA-Z0-9&'!\\-] ?)*[a-zA-Z0-9&'!\\-]$") // only the middle characters may be a space and no consecutive spaces
     String name;
 
     @NotNull(message = "The lat must not be blank")
