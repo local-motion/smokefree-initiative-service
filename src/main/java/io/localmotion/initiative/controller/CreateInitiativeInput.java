bfaa67e2b41e0f818a5acb82eab79be243b7162a
Dimitri Dekker <dimitri_dekker@hotmail.com>
2019-06-06 07:31:44 +0200

    Playground name validation (#127)
    * added regex on playground name validation

--- src/main/java/io/localmotion/initiative/controller/CreateInitiativeInput.java
+++ src/main/java/io/localmotion/initiative/controller/CreateInitiativeInput.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -20,9 +22,11 @@ class CreateInitiativeInput {
     @Size(min = 1, message = "The initiativeId must not be blank")
     String initiativeId;
 
+    @NotBlank
     @Length(min = 3, message = "The name must be at least 3 characters")
     @Length(max = 40, message = "The name must be less than 40 characters")
-    @NotNull
+//    @Pattern(regexp="^[a-zA-Z0-9&'!\\-][ a-zA-Z0-9&'!\\-]{0,38}[a-zA-Z0-9&'!\\-]$") // only the middle characters may be a space
+    @Pattern(regexp="^([a-zA-Z0-9&'!\\-] ?)*[a-zA-Z0-9&'!\\-]$") // only the middle characters may be a space and no consecutive spaces
     String name;
 
     @NotNull(message = "The lat must not be blank")
